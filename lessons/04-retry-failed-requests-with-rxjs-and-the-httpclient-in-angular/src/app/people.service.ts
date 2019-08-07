245cdb569395442e8ac12d66ec7f19df5b6a7ed9
Zac Jones <zacjones93@gmail.com>
2019-08-07 16:15:57 -0400

    Update RxJS imports in people.service.ts

--- lessons/04-retry-failed-requests-with-rxjs-and-the-httpclient-in-angular/src/app/people.service.ts
+++ lessons/04-retry-failed-requests-with-rxjs-and-the-httpclient-in-angular/src/app/people.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
-import 'rxjs/add/operator/retry';
-import 'rxjs/add/operator/retry';
-import 'rxjs/add/operator/retryWhen';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/observable/of';
+import { Observable, of, throwError, } from 'rxjs';
+import { delay, retryWhen, flatMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class PeopleService {
